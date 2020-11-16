c455946595173515af8dc61c1f7bcf7e561de766
Josh Seamans <josh@seamans.me>
2020-11-15 18:07:07 -0600

    added unit test for file validator, also fixed spelling error, lastly removed redundant code.

--- django_airavata/scripts/pdbParser.py
+++ django_airavata/scripts/pdbParser.py
@@ -1,3 +1,4 @@
+from django.test import TestCase
 from PDBParser.myPDBParser import PDBParser
 
 
@@ -11,25 +12,39 @@ def validate_pdb(file_name):
         atomCount = 0
         for atom in atoms:
             atomCount += 1
-        print("Anount of atoms: " + str(atomCount))
+        print("Amount of atoms: " + str(atomCount))
         return True
     except (Exception) as e:
         print("File contains irregularities, Running with permissive mode.")
         print(e)
-        try:
-            parser = PDBParser(PERMISSIVE=1)
-            structure_id = "inputFile"
-            filename = file_name
-            parser.get_structure(structure_id, filename)
-            return True
-        except (Exception) as e:
-            print("File contains errors.")
-            print(e)
-            return False
+        return False
+
+
+class TestValidator(TestCase):
+
+    # No errors
+    def test_ValidPDB(self):
+
+        self.assertEqual(True, validate_pdb('testPDBs/input1.pdb'))
+
+    # Line partially Missing
+    def test_NonValidPDB(self):
+
+        self.assertEqual(False, validate_pdb('testPDBs/input2.pdb'))
+
+    # Space mid line
+    def test_ParsingErrorValidPDB(self):
+
+        self.assertEqual(False, validate_pdb('testPDBs/input3.pdb'))
+
+    # Space start of line
+    def test_ParsingMidLineErrorPDB(self):
+
+        self.assertEqual(True, validate_pdb('testPDBs/input4.pdb'))
 
 
 def main():
-    validate_pdb("1re2.pdb")
+    validate_pdb("input1.pdb")
     return 0
 
 
