a156814d36b9534d8ef8c4ca0607d4f5fbcab53a
evan-palmer <evanp922@gmail.com>
2021-03-02 12:39:57 -0600

    Implemented changes to the molecule display

---
+++ django_airavata/apps/workspace/static/django_airavata_workspace/js/components/experiment/output-displays/MoleculeOutputDisplay.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="molecule" style="width: 500px; height: 500px; background-color: black;"></div> 
+</template>
+
+<script>
+export default {
+  name: "molecule-output-display",
+  props: {
+    viewData: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    dataContainer() {
+      if (this.viewData) {
+        let transformer = new ChemDoodle.TransformCanvas3D('transformer', 500, 500, true);
+        transformer.styles.set3DRepresentation('Ball and Stick');
+        transformer.styles.backgroundColor = 'black';
+
+        ChemDoodle.io.file.content(this.viewData, function(fileContent) {
+          var mol = ChemDoodle.readPDB(fileContent);
+          transformer.loadMolecule(mol);
+        }); 
+        
+        return transformer;
+      } else {
+        return null;
+      }
+    },
+  },
+};
+</script>
