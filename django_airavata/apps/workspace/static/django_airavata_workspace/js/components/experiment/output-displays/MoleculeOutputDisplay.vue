50b4ea5271d1bf75b678de0a2be54dce30483b67
seamansJosh <josh.seamans@huskers.unl.edu>
2021-03-25 14:21:36 -0500

    Added 1 item checks for dropdown and scripts and css for GLMol get added when GLMol is wanted to be viewed

--- django_airavata/apps/workspace/static/django_airavata_workspace/js/components/experiment/output-displays/MoleculeOutputDisplay.vue
+++ django_airavata/apps/workspace/static/django_airavata_workspace/js/components/experiment/output-displays/MoleculeOutputDisplay.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="width: 500px; height: 500px; background-color: white;"  >
+  <div>
     {{ dataContainer }}
     <div id="container"></div>
     <canvas id="transformBallAndStick"></canvas>
@@ -8,7 +8,6 @@
 
 <script>
   /* globals ChemDoodle */
-
   export default {
     name: "molecule-output-display",
     description: "Molecule Viewer",
@@ -18,8 +17,25 @@
         required: true,
       },
     },
+    mounted: function(){
+      this.$nextTick(function(){
+        var link = document.createElement('link');
+        link.rel = "styleSheet";
+        link.href = "../../../static/glmol/ChemDoodleWeb.css"
+        link.type = "text/css";
+
+        var script = document.createElement('script');
+        script.type = "text/javascript";
+        script.src = "../../../static/glmol/ChemDoodleWeb.js";
+        
+        var head = document.getElementsByTagName("head")[0];
+        
+        head.append(script);
+        head.append(link);
+      })
+    },
     computed: {
-      dataContainer() {        
+      dataContainer() {       
         if (this.viewData['pdb']) {
           const files = this.viewData['pdb'];
           let transformBallAndStick = new ChemDoodle.TransformCanvas3D('transformBallAndStick', 500, 500);
@@ -28,40 +44,47 @@
           transformBallAndStick.styles.backgroundColor = 'black';
           let changePdb = function(name) {
             let molecule = ChemDoodle.readPDB(files[name].join(''));
-            try {
-              transformBallAndStick.loadMolecule(molecule);
-            } catch(error) {
-              // // eslint-disable-next-line no-console
-              // console.error(error)
-            }
+            transformBallAndStick.loadMolecule(molecule);
           }
-          var select = document.createElement("select")
-          select.name = "pdbs"
-          select.id = "pdbs"
-          select.onchange = function(value) {
-            changePdb(value.target.selectedOptions[0].label)
+          let size = 0;
+          for (var tempKey in files) {
+              if (files.hasOwnProperty(tempKey)){
+                size++;
+              }
           }
-          
-          let flag = false
-          for (var key in files) {
-            if (files.hasOwnProperty(key)) {      
-              if (!flag){
-                changePdb(key)
-                flag = true
-              }     
-              var option = document.createElement("option");
-              option.value = key;
-              option.text = key;
-              select.appendChild(option);
+
+          // eslint-disable-next-line no-console
+          console.log(size);
+
+          if(size === 1) {
+              changePdb(tempKey);
+          } else {
+            var select = document.createElement("select");
+            select.name = "pdbs";
+            select.id = "pdbs";
+            select.style = "margin-bottom: 1em";
+            select.onchange = function(value) {
+              changePdb(value.target.selectedOptions[0].label);
             }
             
-          }        
-          document.getElementById("container").appendChild(select);
+            let flag = false;
+            for (var key in files) {
+              if (files.hasOwnProperty(key)) {      
+                if (!flag){
+                  changePdb(key);
+                  flag = true;
+                }     
+                var option = document.createElement("option");
+                option.value = key;
+                option.text = key;
+                select.appendChild(option);
+              }
+            }        
+            document.getElementById("container").appendChild(select);
+          }
         }
         return null;
       }
     }
   };
 </script>
-
-n
